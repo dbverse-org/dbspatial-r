4f3f61d267999e61f9b39312c53dc2a1c47e2638
Ed2uiz <ecr7407@gmail.com>
2025-09-23 13:53:01 -0400

    fix .sim_dbSpatial

--- R/sim_dbSpatial.R
+++ R/sim_dbSpatial.R
@@ -1,21 +1,34 @@
 #' Simulate basic dbSpatial object
-#'
+#' @param geom whether to load 'point' or 'polygon' GEOMETRY test file
 #' @return A dbSpatial object
 #' @keywords internal
-.sim_dbSpatial <- function(){
-  test_data = data.frame(
-    x = 1:100,
-    y = 1:100
-  )
-  
+.sim_dbSpatial <- function(geom = c("point", "polygon")){
   con = DBI::dbConnect(duckdb::duckdb(), ":memory:")
+  geom <- match.arg(geom)
+  loadSpatial(con)
   
-  res <- dbSpatial(conn = con,
-                   name = "test_data",
-                   value = test_data,
-                   x_colName = "x",
-                   y_colName = "y",
-                   overwrite = TRUE)
+  if(geom == "point"){
+    test_data = data.frame(
+      x = 1:100,
+      y = 1:100
+    )
+    
+    res <- dbSpatial(conn = con,
+                     name = "test_data",
+                     value = test_data,
+                     x_colName = "x",
+                     y_colName = "y",
+                     overwrite = TRUE)
+  } else {
+    poly_file <- system.file("extdata",
+                             "polygon.geojson",
+                             package = "dbSpatial")
+    
+    res <- dbSpatial(conn = con,
+                     name = "test_data",
+                     value = poly_file,
+                     overwrite = TRUE)
+  }
   
   return(res)
 }
