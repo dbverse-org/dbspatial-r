5561c8b8cca32b072c47105d1bb78198324007bb
Ed2uiz <ecr7407@gmail.com>
2025-09-23 13:53:14 -0400

    feat: st_as_sf

---
+++ R/st_as_sf.R
@@ -0,0 +1,63 @@
+#' @import methods
+#' @importFrom sf st_as_sf st_read
+#' @importFrom dbplyr sql_render remote_con
+#' @importFrom tidyselect everything all_of
+NULL
+
+# Conditional generic registration
+if (!requireNamespace("sf", quietly = TRUE)) {
+  stop("sf package required for spatial conversions", call. = FALSE)
+} else {
+  setGeneric("st_as_sf", package = "sf")
+}
+
+#' Convert dbSpatial objects to sf objects
+#'
+#' @description
+#' S4 method implementation for converting `dbSpatial` objects to `sf` objects.
+#'
+#' @param x A [`dbSpatial`] object to convert
+#' @param geomName \code{character string}. The geometry column name in the  
+#' [`dbSpatial`] object. Default: `"geom"`.
+#' @param select Columns to retain in output (default: all columns)
+#' @param ... Additional arguments passed to [`sf::st_read`]
+#'
+#' @return An [`sf`] object
+#'
+#' @details
+#' This method handles conversion of [`dbSpatial`] to [`sf`] objects using:
+#' - Dynamic column selection via tidyselect semantics
+#' - Automatic geometry column preservation
+#' - SQL-level column subsetting for efficiency
+#'
+#' @family dbSpatial
+#' @export
+setMethod(
+  "st_as_sf",
+  signature = "dbSpatial",
+  function(x, geomName = "geom", select = tidyselect::everything(), ...) {
+    .st_as_sf(x, geomName = geomName, select = {{ select }}, ...)
+  }
+)
+
+#' @keywords internal
+.st_as_sf <- function(x, geomName = "geom", select, ...) {
+  con <- dbplyr::remote_con(x[])
+  .check_con(con)
+  .check_geomName(value = x[], geomName = geomName)
+  
+  # Process column selection with tidy evaluation
+  sql <- x[] |> 
+    dplyr::select({{select}}, tidyselect::all_of(geomName)) |> 
+    dplyr::mutate(!!geomName := ST_AsWKB(!!rlang::sym(geomName))) |>
+    dbplyr::sql_render()
+  
+  res <- sf::st_read(
+    dsn = con,
+    query = sql,
+    geometry_column = geomName,
+    ...
+  )
+  
+  return(res)
+}
