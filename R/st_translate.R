a1ec19a4f9eac509899344d76ec512afb4a185e3
Ed2uiz <ecr7407@gmail.com>
2025-12-29 15:34:38 -0500

    refactor: simplify st_translate implementation

--- R/st_translate.R
+++ R/st_translate.R
@@ -1,39 +1,18 @@
 #' @keywords internal
 .st_translate <- function(dbSpatial, geomName = "geom", dx, dy, ...) {
-  # check inputs
-  tbl <- dbSpatial[]
-  .check_tbl(tbl = tbl)
-  .check_geomName(value = tbl, geomName = geomName)
+  if (missing(dx) || missing(dy)) stop("Please provide dx and dy")
+  if (!is.numeric(dx) || !is.numeric(dy)) stop("dx and dy must be numeric")
 
-  if (missing(dx) | missing(dy)) {
-    stop("Please provide dx and dy")
-  }
-
-  if (!is.numeric(dx) | !is.numeric(dy)) {
-    stop("dx and dy must be numeric")
-  }
-
-  # check to see what geometry type, if polygon then stop
-  geomType <- st_geometrytype(dbSpatial = dbSpatial, geomName = geomName)[] |>
-    head(1) |>
-    dplyr::pull()
-
-  if (geomType != "POINT") {
-    stop("Only POINT geometry is currently supported for st_translate.")
-  }
-
-  # TODO: native translate, no casting
-  res <- tbl |>
-    dplyr::mutate(
-      !!geomName := st_point(
-        st_x(!!rlang::sym(geomName)) + dx,
-        st_y(!!rlang::sym(geomName)) + dy
-      )
-    )
-
-  dbSpatial[] <- res
-
-  return(dbSpatial)
+  # Pass dx and dy explicitly as a named list.
+  # We also provide param_names to ensure correct ordering if we ever changed the list construction.
+  .st_mutate_geom(
+    dbSpatial, 
+    st_func = "ST_Translate", 
+    geomName = geomName, 
+    dest_col = NULL, 
+    args = list(dx = dx, dy = dy),
+    param_names = c("dx", "dy")
+  )
 }
 
 #' @describeIn st_translate Method for `dbSpatial` object
