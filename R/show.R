42ccf05b219d9a8868f3bce84c4a3165843e19a0
Ed2uiz <ecr7407@gmail.com>
2025-12-29 13:15:20 -0500

    feat: add show function for dbSpatial

--- R/show.R
+++ R/show.R
@@ -3,30 +3,51 @@
 #' @name show
 #' @family dbSpatial
 setMethod('show', signature(object = 'dbSpatial'), function(object) {
-  # prepare show
-  # preview <- object[] |> head(1) |> dplyr::collect()
-
-  # geom_name <- .get_geomName(preview)
-
-  # in cases where reading in spatial data and no geom
-  # is present. e.g. parquet file
-  # if(is.null(geom_name)){
-  #   grey_color <- crayon::make_style("grey70")
-  #   cat(grey_color("# Class:    dbSpatial \n"))
-  #   cat(grey_color("# Extent:   NA\n"))
-  #   return(show(object[]))
-  # }
-
-  # extent <- object |>
-  #   st_extent(geomName = geom_name) |>
-  #   unname() |>
-  #   round(2) |> # round to 2 decimal places
-  #   as.character()
-  # labels_str <- paste(paste(extent, collapse = " "), "(xmin xmax ymin ymax)")
-
-  # print metadata
+  # Header
   grey_color <- crayon::make_style("grey60")
   cat(grey_color("# Class:    dbSpatial \n"))
-  # cat(grey_color("# Extent: ", labels_str, paste0("[", geom_name, "]"), "\n"))
-  return(show(object[]))
+  
+  # Access lazy table
+  tbl <- object[]
+  cols <- colnames(tbl)
+  
+  # Heuristic for geometry column
+  geom_col <- if ("geom" %in% cols) "geom" else if ("geometry" %in% cols) "geometry" else NULL
+  
+  if (!is.null(geom_col)) {
+    # Check if column is actually binary/blob (geometry) before wrapping
+    # Some functions like st_is_valid overwrite 'geom' with boolean
+    tryCatch({
+      col_type <- object[] |> 
+        dplyr::select(!!rlang::sym(geom_col)) |> 
+        utils::head(0) |> 
+        dplyr::collect() |> 
+        {function(x) class(x[[1]])}()
+      
+      # If it's a blob/list (raw), treat as geometry and wrap
+      # If it's logical, numeric, character, print as is
+      if ("blob" %in% col_type || "list" %in% col_type || "vctrs_list_of" %in% col_type) {
+         # Get max print length option
+         max_len <- getOption("dbSpatial.max_print", default = 30)
+         
+         # Construct truncation SQL: CASE WHEN length(wkt) > max THEN left(wkt, max) || '...' ELSE wkt END
+         # We use sprintf for cleaner string construction
+         trunc_sql <- sprintf(
+           "CASE WHEN length(ST_AsText(%s)) > %d THEN left(ST_AsText(%s), %d) || '...' ELSE ST_AsText(%s) END",
+           geom_col, max_len, geom_col, max_len, geom_col
+         )
+         
+         display_tbl <- tbl |>
+          dplyr::mutate(!!rlang::sym(geom_col) := dplyr::sql(trunc_sql))
+         print(display_tbl)
+      } else {
+        print(tbl)
+      }
+    }, error = function(e) {
+      # Fallback on error (e.g. connection issues)
+      print(tbl)
+    })
+  } else {
+    print(tbl)
+  }
 })
