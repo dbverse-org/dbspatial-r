80e36a1faa4f56a6cc4fadcfe6fbbf22594df48d
Ed2uiz <ecr7407@gmail.com>
2025-12-29 13:13:40 -0500

    docs: update global options documentation

--- R/global.R
+++ R/global.R
@@ -2,9 +2,16 @@
 #' @description Global options to control package behavior.
 #' @details Use `options()` to set the below options.
 #' @section Options:
+#' * `dbSpatial.max_print`: integer. Max characters for WKT in show method (default 30).
 #' * `dbSpatial.max_mem_convert`: numeric. Max bytes for implicit coercion (default 8GB).
 #' * `dbSpatial.verbose`: logical. Print info messages during coercion (default TRUE).
 #'
+#' @examples
+#' \dontrun{
+#' options(dbSpatial.max_print = 50)
+#' options(dbSpatial.max_mem_convert = 16 * 1024^3)
+#' options(dbSpatial.verbose = FALSE)
+#' }
 #' @name dbSpatial_options
-#' @keywords internal
+#' @aliases dbSpatial-options
 NULL
